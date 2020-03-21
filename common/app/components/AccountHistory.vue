1aaa4b33ffc3b7f1724550c76bf221161017dc70
Martin Guillon <martin.guillon@akylas.fr>
2020-03-21 11:43:55 +0100

    fix: account history better layout

--- common/app/components/AccountHistory.vue
+++ common/app/components/AccountHistory.vue
@@ -1,6 +1,6 @@
 <template>
     <CairnPage @navigatedTo="onLoaded" :title="$t('account_history')" :subtitle="accountInfo.name">
-        <StackLayout slot="actionBarSubView" row='1' col="1" colSpan="2" height="100" marginRight="10">
+        <StackLayout slot="actionBarSubView" row="1" col="1" colSpan="2" height="100" marginRight="10">
             <Label :text="$t('balance') | capitalize" verticalAlignment="center" fontSize="14" color="#88ffffff" />
             <Label verticalAlignment="center" fontSize="30" color="white">
                 <Span :text="accountInfo.balance | currency"></Span>
@@ -12,15 +12,31 @@
                 <CollectionView :items="dataItems" rowHeight="80">
                     <v-template>
                         <GridLayout width="100%" columns="*,auto" rows="auto,*" padding="16" borderBottomWidth="1" borderBottomColor="lightgray" backgroundColor="white">
+                            <Label
+                                fontWeight="bold"
+                                :text="item.credit ? item.debitorName : item.creditorName"
+                                color="black"
+                                verticalAlignment="top"
+                                row="0"
+                                fontSize="14"
+                                maxLines="3"
+                                whiteSpace="nowrap"
+                            />
                             <Label row="1" :text="item.reason" fontSize="14" verticalAlignment="bottom" />
-                            <Label verticalAlignment="top" row="0" fontSize="14" color="#6F6F6F" maxLines="3" whiteSpace="nowrap">
-                                <Span fontWeight="bold" :text="item.credit ? item.debitorName : item.creditorName" color="black" />
-                                <Span :text="item.description ? (' - ' +  item.description) : ''" />
+                            <Label row="2" marginTop="2" v-show="!!item.description" :text="item.description" color="#6F6F6F" fontSize="12" verticalAlignment="bottom" />
 
-                            </Label>
                             <Label col="1" row="0" verticalAlignment="bottom" :text="item.submissionDate | dateRelative" fontSize="14" color="#6F6F6F" horizontalAlignment="right" />
-                            <Label col="1" row="1" horizontalAlignment="right" verticalAlignment="bottom" fontWeight="bold" :text=" item.amount | currency " fontSize="16" :color="item.credit ? accentColor : '#FC5457'">
-                                <Span :text="item.credit ? '+' : '-' "></Span>
+                            <Label
+                                col="1"
+                                row="1"
+                                horizontalAlignment="right"
+                                verticalAlignment="bottom"
+                                fontWeight="bold"
+                                :text="item.amount | currency"
+                                fontSize="16"
+                                :color="item.credit ? accentColor : '#FC5457'"
+                            >
+                                <Span :text="item.credit ? '+' : '-'"></Span>
                                 <Span :text="item.amount | currency"></Span>
                                 <Span :fontFamily="cairnFontFamily" text="cairn-currency" />
                             </Label>
