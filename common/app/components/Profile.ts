cc1859b0efa984023eb149631baa3085b89f6673
Martin Guillon <martin.guillon@akylas.fr>
2020-03-30 17:04:00 +0200

    fix: call with phoneNumber

--- common/app/components/Profile.ts
+++ common/app/components/Profile.ts
@@ -140,7 +140,7 @@ export default class Profile extends PageComponent {
         })
             .then(r => {
                 if (r) {
-                    return this.$authService.deletePhone(phone.id);
+                    return this.$authService.deletePhone(phoneNumber);
                 }
             })
             .catch(this.showError)
