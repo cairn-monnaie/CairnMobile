866c15689c804e237069983ff851b8b3ac846253
Martin Guillon <martin.guillon@akylas.fr>
2020-04-01 16:50:31 +0200

    fix: show gps search location button

--- common/app/components/InteractiveMap.vue
+++ common/app/components/InteractiveMap.vue
@@ -1,13 +1,13 @@
 <template>
     <GridLayout :opacity="opacity">
         <MapComponent ref="mapComp" rowSpan="2" showLocationButton="true" @mapReady="onMapReady" @mapStable="onMapStable" @elementClick="onElementClick" />
+        <GridLayout verticalAlignment="bottom" :translateY="-bottomSheetTranslation" :opacity="scrollingWidgetsOpacity">
+            <MDButton @tap="askUserLocation" class="floating-btn" margin="8" text="mdi-crosshairs-gps" horizontalAlignment="right" verticalAlignment="bottom" />
+        </GridLayout>
         <BottomSheetHolder rowSpan="2" ref="bottomSheetHolder" :peekerSteps="bottomSheetSteps" isPassThroughParentEnabled="true" @close="unselectItem" @scroll="onBottomSheetScroll">
-            <GridLayout :paddingBottom="bottomSheetTranslation" :opacity="scrollingWidgetsOpacity">
-                <MDButton @tap="askUserLocation" class="floating-btn" margin="8" text="mdi-crosshairs-gps" horizontalAlignment="right" verticalAlignment="bottom" />
-            </GridLayout>
             <MapBottomSheet slot="bottomSheet" :item="selectedItem" :steps="bottomSheetSteps" />
         </BottomSheetHolder>
-            <MDActivityIndicator v-show="loading" row="1" :busy="{ loading }" verticalAlignment="center" horizontalAlignment="center" />
+        <MDActivityIndicator v-show="loading" row="1" :busy="{ loading }" verticalAlignment="center" horizontalAlignment="center" />
     </GridLayout>
 </template>
 
