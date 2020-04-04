37013ab3ecc22e2b1ca3c737ad79742faec0e6b7
Martin Guillon <martin.guillon@akylas.fr>
2020-04-04 11:38:26 +0200

    fix: no elevation needed ere

--- common/app/components/MapBottomSheet.vue
+++ common/app/components/MapBottomSheet.vue
@@ -1,5 +1,5 @@
 <template>
-        <GridLayout ref="bottomSheet" backgroundColor="white" elevation="1" :rows="rows">
+        <GridLayout ref="bottomSheet" backgroundColor="white" :rows="rows">
             <!-- <transition name="fade" duration="200"> -->
                 <ListItem v-if="!!item" :showBottomLine="false" :avatar="item.image" :title=" item.name" :subtitle="item.description" rightButton="mdi-chevron-right" @rightTap="showProfile(item)"/>
             <!-- </transition> -->
