c570128b3dc26f91b1458bb3c030bb231dd5d16a
Martin Guillon <martin.guillon@akylas.fr>
2020-04-09 14:11:28 +0200

    chore: removed unused

--- common/app/vue.views.ts
+++ common/app/vue.views.ts
@@ -7,9 +7,9 @@ import { Label as HTMLLabel } from 'nativescript-htmllabel'; // require first to
 import ActivityIndicatorPlugin from 'nativescript-material-activityindicator/vue';
 import ButtonPlugin from 'nativescript-material-button/vue';
 // import CardViewPlugin from 'nativescript-material-cardview/vue';
-import ProgressPlugin from 'nativescript-material-progress/vue';
+// import ProgressPlugin from 'nativescript-material-progress/vue';
 // import RipplePlugin from 'nativescript-material-ripple/vue';
-import SliderPlugin from 'nativescript-material-slider/vue';
+// import SliderPlugin from 'nativescript-material-slider/vue';
 import TextFieldPlugin from 'nativescript-material-textfield/vue';
 import BottomSheetPlugin from 'nativescript-material-bottomsheet/vue';
 import CartoPlugin from 'nativescript-carto/vue';
@@ -32,9 +32,9 @@ const Plugin = {
         Vue.use(ImagePlugin);
         Vue.use(ButtonPlugin);
         // Vue.use(CardViewPlugin);
-        Vue.use(ProgressPlugin);
+        // Vue.use(ProgressPlugin);
         // Vue.use(RipplePlugin);
-        Vue.use(SliderPlugin);
+        // Vue.use(SliderPlugin);
         Vue.use(TextFieldPlugin);
         Vue.use(BottomSheetPlugin);
         Vue.use(CartoPlugin);
