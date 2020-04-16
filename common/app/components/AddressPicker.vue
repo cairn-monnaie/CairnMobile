1a79756a02cc13eb5cef52d0829f90765aab75bd
Martin Guillon <martin.guillon@akylas.fr>
2020-04-16 16:45:41 +0200

    chore: refactoring

--- common/app/components/AddressPicker.vue
+++ common/app/components/AddressPicker.vue
@@ -4,8 +4,8 @@
             <GridLayout>
                 <GridLayout class="pageContent" rows="auto,*">
                     <StackLayout class="flatCardView"  orientation="horizontal" height="50" margin="10">
-                        <MDButton variant="flat" color="gray" class="icon-themed-btn" text="mdi-close" @tap="close()" />
-                        <MDTextField width="100%" class="input" floating="false" fontSize="17" variant="none" ref="textField" color="black" height="50" :hint="$t('address') | capitalize" verticalAlignment="center" @focus="onFocus" @blur="onBlur" @textChange="onTextChange" paddingRight="5" @loaded="onTFLoaded" />
+                        <Button variant="flat" color="gray" class="icon-themed-btn" text="mdi-close" @tap="close()" />
+                        <TextField width="100%" class="input" floating="false" fontSize="17" variant="none" ref="textField" color="black" height="50" :hint="$t('address') | capitalize" verticalAlignment="center" @focus="onFocus" @blur="onBlur" @textChange="onTextChange" paddingRight="5" @loaded="onTFLoaded" />
                         <MDActivityIndicator v-show="loading" busy class="activity-indicator" width="20" height="20" />
                     </StackLayout>
                     <CollectionView row="1" :items="dataItems" rowHeight="80">
