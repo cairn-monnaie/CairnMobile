7299fc6284322c8ec5d9cd1477879b81d8efa814
Martin Guillon <martin.guillon@akylas.fr>
2020-04-16 16:40:52 +0200

    fix: faster third parties list view

--- common/app/components/ThirdPartySoftwareBottomSheet.vue
+++ common/app/components/ThirdPartySoftwareBottomSheet.vue
@@ -1,11 +1,17 @@
 <template>
     <GridLayout height="400" @shownInBottomSheet="onShownInBottomSheet()" borderRadius="10">
-        <CollectionView :items="dataItems" rowHeight="80">
+        <CollectionView :items="dataItems" rowHeight="60">
             <v-template>
-                <ListItem :title="item.moduleName" :subtitle="item.moduleUrl" :date="item.moduleVersion" @tap="onTap(item)" />
+                <GridLayout @tap="onTap(item)" class="listitem" padding="10">
+                    <StackLayout class="title-holder">
+                        <Label class="title" :text="item.moduleName" />
+                        <Label class="subtitle" :text="item.moduleUrl" />
+                    </StackLayout>
+                </GridLayout>
+                <!-- <ListItem :title="item.moduleName" :subtitle="item.moduleUrl" :date="item.moduleVersion" @tap="onTap(item)" /> -->
             </v-template>
         </CollectionView>
     </GridLayout>
-</template>
+</template> 
 
 <script src="./ThirdPartySoftwareBottomSheet.ts" />
