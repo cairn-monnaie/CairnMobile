0775f71bcf731646a0198b40111d2db2cb1f5412
Martin Guillon <martin.guillon@akylas.fr>
2020-04-27 15:26:51 +0200

    chore: about open links in-app if possible

--- common/app/components/About.ts
+++ common/app/components/About.ts
@@ -4,8 +4,9 @@ import PageComponent from '~/components/PageComponent';
 import SettingLabelIcon from '~/components/SettingLabelIcon';
 import ThirdPartySoftwareBottomSheet from '~/components/ThirdPartySoftwareBottomSheet';
 import { ComponentIds } from './App';
-import { showSnack } from 'nativescript-material-snackbar';
 import { share } from '~/utils/share';
+import InAppBrowser from 'nativescript-inappbrowser';
+import { primaryColor } from '~/variables';
 
 @Component({
     components: {
@@ -27,20 +28,69 @@ export default class About extends PageComponent {
     }
 
     showThirdPartySoftwares() {}
-
+    async openLink(url: string) {
+        try {
+            const available = await InAppBrowser.isAvailable();
+            if (available) {
+                const result = await InAppBrowser.open(url, {
+                    // iOS Properties
+                    dismissButtonStyle: 'close',
+                    preferredBarTintColor: primaryColor,
+                    preferredControlTintColor: 'white',
+                    readerMode: false,
+                    animated: true,
+                    // modalPresentationStyle: 'fullScreen',
+                    // modalTransitionStyle: 'partialCurl',
+                    // modalEnabled: true,
+                    enableBarCollapsing: false,
+                    // Android Properties
+                    showTitle: true,
+                    toolbarColor: primaryColor,
+                    secondaryToolbarColor: 'white',
+                    enableUrlBarHiding: true,
+                    enableDefaultShare: true,
+                    forceCloseOnRedirection: false
+                    // Specify full animation resource identifier(package:anim/name)
+                    // or only resource name(in case of animation bundled with app).
+                    // animations: {
+                    //     startEnter: 'slide_in_right',
+                    //     startExit: 'slide_out_left',
+                    //     endEnter: 'slide_in_left',
+                    //     endExit: 'slide_out_right'
+                    // },
+                    // headers: {
+                    //     'my-custom-header': 'my custom header value'
+                    // }
+                });
+                // alert({
+                //     title: 'Response',
+                //     message: JSON.stringify(result),
+                //     okButtonText: 'Ok'
+                // });
+            } else {
+                openUrl(url);
+            }
+        } catch (error) {
+            alert({
+                title: 'Error',
+                message: error.message,
+                okButtonText: 'Ok'
+            });
+        }
+    }
     onTap(command: string) {
         switch (command) {
             case 'privacy':
-                openUrl(PRIVACY_POLICY_URL);
+                this.openLink(PRIVACY_POLICY_URL);
                 break;
             case 'terms':
-                openUrl(TERMS_CONDITIONS_URL);
+                this.openLink(TERMS_CONDITIONS_URL);
                 break;
             case 'support':
-                openUrl(SUPPORT_URL);
+                this.openLink(SUPPORT_URL);
                 break;
             case 'github':
-                openUrl(GIT_URL);
+                this.openLink(GIT_URL);
                 break;
             case 'share':
                 share({
@@ -52,8 +102,8 @@ export default class About extends PageComponent {
                 break;
             case 'third_party':
                 this.$showBottomSheet(ThirdPartySoftwareBottomSheet, {
-                    ignoreTopSafeArea:true,
-                    trackingScrollView:'trackingScrollView'
+                    ignoreTopSafeArea: true,
+                    trackingScrollView: 'trackingScrollView'
                 });
                 break;
         }
