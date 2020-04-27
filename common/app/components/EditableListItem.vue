512fdfcd9b4dba0472e776eed663ff8d576737b9
Martin Guillon <martin.guillon@akylas.fr>
2020-04-27 15:25:35 +0200

    fix: Editable fields not working

---
+++ common/app/components/EditableListItem.vue
@@ -0,0 +1,8 @@
+<template>
+<GridLayout columns="16,auto,*,auto,16" rows="5,auto,*,auto,5" backgroundColor="white">
+    <Label v-if="!!leftIcon" col="1" row="0" rowSpan="5" fontSize="24" marginRight="16" textAlignment="left"  :text="leftIcon" horizontalAlignment="left" verticalAlignment="center" color="${iconColor}" class="mdi" />
+    <TextField col="2" row="2" fontSize="17" :text="title" :floatingLabel="overText" :hint="subtitle || overText" @textChange="$emit('textChange', $event)" @return="$emit('return', $event)"/>
+    <Button variant="flat" col="3" row="2" rowSpan="1" v-if="!!rightButton" class="icon-themed-btn"  :text="rightButton" verticalAlignment="center" @tap="$emit('rightTap')"/>
+</GridLayout>
+</template>
+<script lang="ts" src="./EditableListItem.ts"/>
