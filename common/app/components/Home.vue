bf3a1982c9482f8aeecccec415a9e626f41712c8
Martin Guillon <martin.guillon@akylas.fr>
2020-05-05 15:09:51 +0200

    fix: label about new pros

--- common/app/components/Home.vue
+++ common/app/components/Home.vue
@@ -10,12 +10,25 @@
         :mdiActionFontSize="40"
         @actionTap="openTransferWindow"
     >
-        <StackLayout>
-            <Pager :items="users" height="30%" :backgroundColor="themeColor" showIndicator>
+        <GridLayout rows="auto,auto,*">
+            <Label
+                verticalTextAlignment="top"
+                class="list_section_subtitle"
+                color="white"
+                :text="$t('discover_your_pros')"
+                :backgroundColor="themeColor"
+            />
+            <Pager row="1" :items="users" height="30%" :backgroundColor="themeColor" showIndicator>
                 <v-template>
                     <GridLayout rows="*" columns="*" @tap="showProfile(item)">
                         <!-- <MapComponent v-show="item.address && item.address.latitude" rowSpan="4" opacity="0.5" /> -->
-                        <NSImg stretch="center" :src="item.image || DEFAULT_IMAGE_URL" width="100%" height="100%" backgroundColor="#E86A45" />
+                        <NSImg
+                            stretch="center"
+                            :src="item.image || DEFAULT_IMAGE_URL"
+                            width="100%"
+                            height="100%"
+                            backgroundColor="#E86A45"
+                        />
                         <Label
                             textAlignment="left"
                             verticalAlignment="bottom"
@@ -24,23 +37,38 @@
                             fontSize="14"
                             color="white"
                         >
-                        <Span fontSize="16" fontWeight="bold" :text="item.name + '\n'"/>
-                        <Span :text="item.description + '\n' + formatAddress(item.address)"/>
+                            <Span fontSize="16" fontWeight="bold" :text="item.name + '\n'" />
+                            <Span :text="item.description + '\n' + formatAddress(item.address)" />
                         </Label>
                     </GridLayout>
                 </v-template>
             </Pager>
-            <PullToRefresh height="70%" @refresh="refresh">
+            <PullToRefresh row="2" @refresh="refresh">
                 <CollectionView :items="accounts" rowHeight="180">
                     <v-template>
                         <StackLayout>
-                            <GridLayout class="cardView" margin="20" padding="10" columns="*, auto" rows="auto, *" @tap="onItemTap(item)">
+                            <GridLayout
+                                class="cardView"
+                                margin="20"
+                                padding="10"
+                                columns="*, auto"
+                                rows="auto, *"
+                                @tap="onItemTap(item)"
+                            >
                                 <Label row="0" :text="item.name | capitalize" fontWeight="bold" fontSize="18" />
-                                <Label row="1" class="balance"  paddingTop="20">
+                                <Label row="1" class="balance" paddingTop="20">
                                     <Span :text="item.balance | currency(true)" />
                                     <Span :fontFamily="cairnFontFamily" text="cairn-currency" />
                                 </Label>
-                                <Label col="1" rowSpan="2" class="mdi" text="mdi-chevron-right" fontSize="30" color="gray" verticalAlignment="center" />
+                                <Label
+                                    col="1"
+                                    rowSpan="2"
+                                    class="mdi"
+                                    text="mdi-chevron-right"
+                                    fontSize="30"
+                                    color="gray"
+                                    verticalAlignment="center"
+                                />
                             </GridLayout>
                         </StackLayout>
                     </v-template>
@@ -50,7 +78,7 @@
                 <FabItem :title="$t('transfer') | capitalize" iconClass="mdi" icon="mdi-bank-transfer" @tap="openTransferWindow" />
                 <FabItem :title="$t('add_beneficiary') | capitalize" iconClass="mdi" icon="mdi-account-plus" @tap="addBeneficiary" />
             </Fab> -->
-        </StackLayout>
+        </GridLayout>
         <StackLayout slot="bottomActionBarRightButtons" verticalAlignment="center" orientation="horizontal">
             <Button variant="flat" class="icon-btn" text="mdi-qrcode-scan" @tap="scanQRCode" />
         </StackLayout>
